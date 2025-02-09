9a7b7054741628929bad0b2cd8afd08d869bffd4
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-02-09 17:17:16 +0100

    feat: create handlers for books, authors and genres routes

--- src/handlers/authors-handler.ts
+++ src/handlers/authors-handler.ts
@@ -1,6 +1,23 @@
 // External Imports
 import type { Handler } from "hono";
+import { StatusCodes } from "http-status-codes";
 
-export const getAllAuthors: Handler = (c) => {
-  return c.text(`GET - ${c.req.path}`);
+// Local Imports
+import { getAllAuthorsQ } from "@/lib/db/authors.js";
+import { errorResponse, successResponse } from "@/lib/utils/api-response.js";
+
+export const getAllAuthors: Handler = async (c) => {
+  try {
+    const authors = await getAllAuthorsQ();
+    return c.json(
+      successResponse("Authors successfully retrieved.", authors),
+      StatusCodes.OK,
+    );
+  } catch (error) {
+    console.error("Error retrieving authors:", error);
+    return c.json(
+      errorResponse("INTERNAL_SERVER_ERROR", "Error retrieving authors."),
+      StatusCodes.INTERNAL_SERVER_ERROR,
+    );
+  }
 };
--- src/handlers/genres-handler.ts
+++ src/handlers/genres-handler.ts
@@ -1,6 +1,23 @@
 // External Imports
 import type { Handler } from "hono";
+import { StatusCodes } from "http-status-codes";
 
-export const getAllGenres: Handler = (c) => {
-  return c.text(`GET - ${c.req.path}`);
+// Local Imports
+import { getAllGenresQ } from "@/lib/db/genres.js";
+import { errorResponse, successResponse } from "@/lib/utils/api-response.js";
+
+export const getAllGenres: Handler = async (c) => {
+  try {
+    const genres = await getAllGenresQ();
+    return c.json(
+      successResponse("Genres successfully retrieved.", genres),
+      StatusCodes.OK,
+    );
+  } catch (error) {
+    console.error("Error retrieving genres:", error);
+    return c.json(
+      errorResponse("INTERNAL_SERVER_ERROR", "Error retrieving genres."),
+      StatusCodes.INTERNAL_SERVER_ERROR,
+    );
+  }
 };
