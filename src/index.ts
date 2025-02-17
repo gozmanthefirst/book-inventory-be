5b0353cc71d846cac0704bcd6331c76f899f013b
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-02-17 20:37:40 +0100

    refactor: remove better auth

--- src/index.ts
+++ src/index.ts
@@ -7,33 +7,22 @@ import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 
 // Local Imports
-import { authenticate } from "./middlewares/authenticate.js";
 import { errorHandler } from "./middlewares/error-handler.js";
 import { notFoundRoute } from "./middlewares/not-found-route.js";
 import authorsRouter from "./routes/authors-route.js";
 import booksRouter from "./routes/books-route.js";
 import genresRouter from "./routes/genres-route.js";
-import { auth } from "./services/auth.js";
 
 dotenv.config();
 
 // Initialize app
-const app = new Hono<{
-  Variables: {
-    user: typeof auth.$Infer.Session.user | null;
-    session: typeof auth.$Infer.Session.session | null;
-  };
-}>({ strict: false });
+const app = new Hono({ strict: false });
 
 // Enable CORS
 app.use(
   "*",
   cors({
     origin: ["https://books.gozman.dev", "http://localhost:3000"],
-    allowHeaders: ["Content-Type", "Authorization"],
-    allowMethods: ["GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS"],
-    exposeHeaders: ["Content-Length"],
-    credentials: true,
     maxAge: 3600,
   }),
 );
@@ -45,15 +34,7 @@ app.use(logger());
 // Healthcheck
 app.get("/", (c) => c.text("API is up and running!"));
 
-// Setup Better Auth
-app.on(["POST", "GET"], "/api/v1/auth/**", (c) => auth.handler(c.req.raw));
-
-//! Unprotected routes go here!
-
-// Enable auth protection
-app.use(authenticate);
-
-// Protected Routes
+// Routes
 app.route("/api/v1/books", booksRouter);
 app.route("/api/v1/authors", authorsRouter);
 app.route("/api/v1/genres", genresRouter);
