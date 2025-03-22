611f156d06012abb80a75b0f2d96d72dd36315ab
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-03-22 22:11:51 +0100

    chore: remove unwanted utility functions

--- src/utils/number.ts
+++
@@ -1,4 +0,0 @@
-export const roundUp = (number: number): number => Math.ceil(number);
-
-export const roundToTwoDecimals = (number: number): number =>
-  Math.round(number * 100) / 100;
--- src/utils/string.ts
+++
@@ -1,3 +0,0 @@
-export const removeDashesAndSpaces = (input: string): string => {
-  return input.replace(/[-\s]/g, "");
-};
